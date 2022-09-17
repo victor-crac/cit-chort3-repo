2e9d85fd3c25ca430138ea7b27dc6ca76b2e38d9
victor-crac <82319700+victor-crac@users.noreply.github.com>
2022-09-17 13:14:06 +0300

    BETTER COMPREHENSION OF THE PROBLEM AT HAND

--- OO Python/week5bank.py
+++ OO Python/week5bank.py
@@ -1,17 +1,17 @@
 from ast import Str
 from unicodedata import name
+# from unicodedata import name
 
 
 class BankAccount:
-    def __init__(self ,acccountNumber,balance,owner,type) -> None:
+    def __init__(self ,accountNumber,balance,owner,type) -> None:
     
-     self.a =acccountNumber 
+     self.a =accountNumber 
      self.b =balance
      self.owner =owner
      self.type =type
-    
-    def __repr__(self) -> str:
-        f'This A/C NO{a} with balance of {} is a {} and owned by {}'
+    def __str__(self) -> str:
+        return 'the {} account is a {} type of account, is owned by {} and has a balance of {} '.format(self.a,self.type,self.owner,self.b)
 
 class Bank:
     def __init__(self ,name , accounts) -> None:
@@ -22,29 +22,60 @@ class Bank:
          self.bankAccount.append(b)
 
     def __repr__(self) -> str:
-        f'This  is {name} bank'
+          return f'This  is {self.name} bank'
 
 class Customer:
     def __init__(self , name , accounts) -> None:
-        self.customerName =name
+        self.name =name
         self.accounts = accounts
 
-    def bankAccount(self ,b):
+    def addBankAccount(self ,b):
          self.bankAccount.append(b)
 
     def __repr__(self) -> str:
-        f'Hello {name}'
+        return f'Hello {self.name}'
 class Transaction:
     def __init__(self , account ,amount ,type) -> None:
 
-        self.account=account
+# print(customer_A)
         self.amount =amount
         self.type=type
 
 equity = Bank('EQUITY BANK' , accounts=[])
 customer_A =Customer('MUSINGUZI DOUGLAS' , accounts=[])
-bankAccount =BankAccount('de0992872' ,'546567' , 'Douglas' , 'Fixed DEposit')
+bank =BankAccount('de0992872' ,546567 , 'Douglas' , 'Fixed DEposit')
+# print(equity)
+# print(customer_A)
+print(customer_A.__repr__())
+print(equity.__repr__())
+print(bank.__str__())
 
+def create_bank(name,accounts,bank_obj):
+    bank_obj = Bank(name)
+    for account in accounts:
+        bank_obj.addBankAccounts(account)
+    return bank_obj
+
+# Creting new customer object
+
+def create_customer(name,accounts,cust_obj):
+    cust_obj = Bank(name)
+    for account in accounts:
+        cust_obj.addBankAccounts(account)
+
+# new bankAccount object
+
+def create_BankAccount(accountNumber,balance,owner,type,bankAcc_obj):
+    bankAcc_obj = BankAccount(accountNumber,balance,owner,type)
+
+# create a transaction object
+
+def create_Transaction(amount,type,account,trans_obj,bankAcc_obj):
+    trans_obj = Transaction(amount,type)
+    trans_obj.add_account(create_BankAccount)
+
+
+   
 
 
         
